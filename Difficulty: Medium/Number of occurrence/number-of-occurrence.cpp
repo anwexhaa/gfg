802f87cb9b933ff6d624ae10c2b0e11ab7effb96
Anwesha Das <150115435+anwexhaa@users.noreply.github.com>
2024-09-03 11:04:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Medium/Number of occurrence/number-of-occurrence.cpp
@@ -12,37 +12,47 @@ public:
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int start=0, end=n-1, first=-1, last=-1, mid;
+	    int start=0,end=n-1,mid,first=-1,last=-1;
+	    
 	    while(start<=end)
 	    {
 	        mid=start+(end-start)/2;
-	        if(arr[mid]==x){
-	        first=mid;
-	        end=mid-1;
+	        if(arr[mid]==x)
+	        {
+	            first=mid;
+	            end=mid-1;
 	        }
 	        else if(arr[mid]<x)
-	        start=mid+1;
+	        {
+	            start=mid+1;
+	        }
 	        else
-	        end=mid-1;
+	        {
+	            end=mid-1;
+	        }
 	    }
-	        if(first==-1) return 0;
-	        start=0, end=n-1;
-	        while(start<=end)
+	    start=0;
+	    end=n-1;
+	    if(first==-1) return 0;
+	    
+	    while(start<=end)
 	    {
 	        mid=start+(end-start)/2;
-	        if(arr[mid]==x){
-	        last=mid;
-	        start=mid+1;
+	        if(arr[mid]==x)
+	        {
+	            last=mid;
+	            start=mid+1;
 	        }
 	        else if(arr[mid]<x)
-	        start=mid+1;
+	        {
+	            start=mid+1;
+	        }
 	        else
-	        end=mid-1;
-	        
+	        {
+	            end=mid-1;
+	        }
 	    }
-	    
-	
-	  return last-first+1;
+	    return last - first + 1;
 	}
 };
 
