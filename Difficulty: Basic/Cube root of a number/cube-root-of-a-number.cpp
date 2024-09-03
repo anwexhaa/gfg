6cb96725941b751c3a1d0a79be2cd465bbd47b93
Anwesha Das <150115435+anwexhaa@users.noreply.github.com>
2024-09-03 10:33:38 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Basic/Cube root of a number/cube-root-of-a-number.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int cubeRoot(int N) {
+        // code here
+        if(N<2)
+        return N;
+        int start=0, end=N,mid,ans;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            double cube=mid*mid;
+            if(mid==N/cube)
+            {
+                ans=mid;
+                break;
+            }
+            else if(mid<N/cube)
+            {
+                ans=mid;
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.cubeRoot(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
