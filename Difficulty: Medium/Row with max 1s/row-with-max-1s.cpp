0be30c4cf3b51460c438585e24ec1a7bdfc23099
Anwesha Das <150115435+anwexhaa@users.noreply.github.com>
2025-03-04 07:40:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -4,43 +4,97 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function template for C++
 class Solution {
   public:
-    int rowWithMax1s(vector<vector<int> > &arr) {
+    int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+    int rowWithMax1s(vector<vector<int>> &matrix) {
         // code here
-       int n=arr.size(), m=arr[0].size();
-       int j=m-1;
-       int one=-1;
-       
-       for(int i=0;i<n;i++)
-       {
-           while(j>=0 && arr[i][j]==1)
-           {
-               j--;
-               one=i;
-           }
-       }
-       return one;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int cnt_max = 0;
+    int index = -1;
+
+    //traverse the rows:
+    for (int i = 0; i < n; i++) {
+        // get the number of 1's:
+        int cnt_ones = m - lowerBound(matrix[i], m, 1);
+        if (cnt_ones > cnt_max) {
+            cnt_max = cnt_ones;
+            index = i;
+        }
+    }
+    return index;
     }
 };
 
+
 //{ Driver Code Starts.
+
 int main() {
     int t;
     cin >> t;
+    cin.ignore(); // Ignore the newline character after the integer input
     while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int> > arr(n, vector<int>(m));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> arr[i][j];
+        string input;
+        getline(cin, input);
+        vector<vector<int>> mat;
+        string innerArray;
+        bool isInsideArray = false;
+
+        for (char c : input) {
+            if (c == '[') {
+                if (isInsideArray) {
+                    innerArray.clear();
+                }
+                isInsideArray = true;
+            } else if (c == ']') {
+                if (isInsideArray && !innerArray.empty()) {
+                    vector<int> row;
+                    stringstream ss(innerArray);
+                    int num;
+
+                    while (ss >> num) {
+                        row.push_back(num);
+                        if (ss.peek() == ',')
+                            ss.ignore();
+                        while (isspace(ss.peek()))
+                            ss.ignore();
+                    }
+
+                    mat.push_back(row);
+                    innerArray.clear();
+                }
+                isInsideArray = false;
+            } else if (isInsideArray) {
+                if (!isspace(c)) {
+                    innerArray += c;
+                }
             }
         }
-        Solution ob;
-        auto ans = ob.rowWithMax1s(arr);
-        cout << ans << "\n";
+
+        Solution obj;
+        cout << obj.rowWithMax1s(mat);
+        cout << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
