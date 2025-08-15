31046b274ff3def7afe55ce519c4adbc0a740220
Anwesha Das <150115435+anwexhaa@users.noreply.github.com>
2025-08-15 10:53:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find length of Loop/find-length-of-loop.cpp
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.cpp
@@ -1,43 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node *next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void printList(Node *node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-void loopHere(Node *head, Node *tail, int position) {
-    if (position == 0)
-        return;
-
-    Node *walk = head;
-    for (int i = 1; i < position; i++)
-        walk = walk->next;
-    tail->next = walk;
-}
-
-
-// } Driver Code Ends
-
 /*
-
-struct Node {
+class Node {
+ public:
     int data;
-    struct Node *next;
+    Node *next;
+
     Node(int x) {
         data = x;
         next = NULL;
@@ -47,20 +13,19 @@ struct Node {
 */
 class Solution {
   public:
-    // Function to find the length of a loop in the linked list.
-    int countNodesinLoop(Node *head) {
+    int lengthOfLoop(Node *head) {
         // Code here
-        Node* first=head;
-        Node* second=head;
-        while(second!=nullptr && second->next!=nullptr)
+        Node* slow=head;
+        Node* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr)
         {
-            first=first->next;
-            second=second->next->next;
-            if(first==second)
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
             {
-                Node* temp=first->next;
                 int count=1;
-                while(temp!=first)
+                Node* temp=slow->next;
+                while(temp!=slow)
                 {
                     count++;
                     temp=temp->next;
@@ -71,38 +36,3 @@ class Solution {
         return 0;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        int k;
-        cin >> k;
-        cin.ignore();
-        struct Node *head = new Node(arr[0]);
-        struct Node *tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            tail->next = new Node(arr[i]);
-            tail = tail->next;
-        }
-        loopHere(head, tail, k);
-
-        Solution ob;
-        cout << ob.countNodesinLoop(head) << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
