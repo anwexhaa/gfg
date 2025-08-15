42847aa369b2e48b4697f7da377f2f3b1f0f0fb4
Anwesha Das <150115435+anwexhaa@users.noreply.github.com>
2025-08-15 08:46:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.cpp
+++ Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.cpp
@@ -1,100 +1,35 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
+/*
+class Node {
+  public:
     int data;
-    struct Node* next;
+    Node* next;
 
-    Node(int x) {
-        data = x;
+    // Default constructor
+    Node() {
+        data = 0;
         next = NULL;
     }
-};
 
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
     }
-    cout << "\n";
-}
-
-
-// } Driver Code Ends
-
-/*Structure of the linked list node is as
-struct Node {
-  int data;
-  struct Node * next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
-}; */
-
+};
+*/
 class Solution {
   public:
     Node *insertAtEnd(Node *head, int x) {
         // Code here
-        if(head==nullptr) 
+        if(head==nullptr) return new Node(x);
+        Node* temp;
+        temp=head;
+        while(temp->next!=nullptr)
         {
-            return new Node(x);
+            temp=temp->next;
         }
-        Node* tail=head;
-        while(tail->next!=nullptr)
-        {
-            tail=tail->next;
-        }
-        Node* temp=new Node(x);
-        tail->next=temp;
+        Node* tail=new Node(x);
+        temp->next=tail;
         return head;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-
-        // Read numbers from the input line
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        int x;
-        cin >> x;
-        cin.ignore();
-
-        Node* head = nullptr;
-
-        // Check if the array is empty
-        if (!arr.empty()) {
-            head = new Node(arr[0]);
-            Node* tail = head;
-            for (size_t i = 1; i < arr.size(); ++i) {
-                tail->next = new Node(arr[i]);
-                tail = tail->next;
-            }
-        }
-
-        Solution ob;
-        Node* ans = ob.insertAtEnd(head, x);
-        printList(ans);
-        cout << "~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
